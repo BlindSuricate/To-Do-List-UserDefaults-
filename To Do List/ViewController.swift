0edaf88b8b6c74020472066dea9927ce592a2ab3
Tarasenko Serhii <blindsuricate@gmail.com>
2019-06-10 04:51:22 +0300

    Initial Commit

---
+++ To Do List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To Do List
+//
+//  Created by user on 2019-06-10.
+//  Copyright © 2019 TarasenkoSerhii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
